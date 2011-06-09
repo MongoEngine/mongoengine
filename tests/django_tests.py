199b4eb860a93c581c1ddfc915f7094fc28de678
Ross Lawley <ross.lawley@gmail.com>
2011-06-09 12:08:37 +0100

    Added django_tests and regression test for order_by
    Refs #190

---
+++ tests/django_tests.py
@@ -0,0 +1,44 @@
+
+# -*- coding: utf-8 -*-
+
+import unittest
+
+from mongoengine import *
+
+
+class QuerySetTest(unittest.TestCase):
+
+    def setUp(self):
+        connect(db='mongoenginetest')
+
+        class Person(Document):
+            name = StringField()
+            age = IntField()
+        self.Person = Person
+
+    def test_order_by_in_django_template(self):
+        """Ensure that QuerySets are properly ordered in Django template.
+        """
+        self.Person.drop_collection()
+
+        self.Person(name="A", age=20).save()
+        self.Person(name="D", age=10).save()
+        self.Person(name="B", age=40).save()
+        self.Person(name="C", age=30).save()
+
+        from django.conf import settings
+        settings.configure()
+        from django.template import Context, Template
+
+        t = Template("{% for o in ol %}{{ o.name }}-{{ o.age }}:{% endfor %}")
+
+        d = {"ol": self.Person.objects.order_by('-name')}
+        self.assertEqual(t.render(Context(d)), u'D-10:C-30:B-40:A-20:')
+        d = {"ol": self.Person.objects.order_by('+name')}
+        self.assertEqual(t.render(Context(d)), u'A-20:B-40:C-30:D-10:')
+        d = {"ol": self.Person.objects.order_by('-age')}
+        self.assertEqual(t.render(Context(d)), u'B-40:C-30:A-20:D-10:')
+        d = {"ol": self.Person.objects.order_by('+age')}
+        self.assertEqual(t.render(Context(d)), u'D-10:A-20:C-30:B-40:')
+
+        self.Person.drop_collection()
