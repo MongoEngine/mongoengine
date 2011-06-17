62c8823e6423d7445dfc011a74d9a748fe0c8d65
Ross Lawley <ross.lawley@gmail.com>
2011-06-17 11:39:53 +0100

    Fixing requirements
    Test requirements are not install requirements!

--- setup.py
+++ setup.py
@@ -45,6 +45,7 @@ setup(name='mongoengine',
       long_description=LONG_DESCRIPTION,
       platforms=['any'],
       classifiers=CLASSIFIERS,
-      install_requires=['pymongo', 'blinker', 'django==1.3'],
+      install_requires=['pymongo'],
       test_suite='tests',
+      tests_require=['blinker', 'django==1.3']
 )
