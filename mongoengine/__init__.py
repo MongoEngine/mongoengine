28a957c6845aa275ea6873009357cc1a23019dec
Ross Lawley <ross.lawley@gmail.com>
2012-05-14 12:43:00 +0100

    Version bump

--- mongoengine/__init__.py
+++ mongoengine/__init__.py
@@ -12,7 +12,7 @@ from signals import *
 __all__ = (document.__all__ + fields.__all__ + connection.__all__ +
            queryset.__all__ + signals.__all__)
 
-VERSION = (0, 6, 8)
+VERSION = (0, 6, 9)
 
 
 def get_version():
