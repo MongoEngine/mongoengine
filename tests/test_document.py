MongoEngine/mongoengine

commit 9e7ea64bd2cc4b42494b574750ff2afa5e8237b8

Fixed db_field load error
Fixes mongoengine/MongoEngine#45

mongoengine/base.py
@@ -798,6 +798,7 @@ class BaseDocument(object):
                     dynamic_data[key] = value
         else:
             for key, value in values.items():
+                key = self._reverse_db_field_map.get(key, key)
                 setattr(self, key, value)
 
         # Set any get_fieldname_display methods

tests/test_document.py
@@ -664,6 +664,26 @@ class DocumentTest(unittest.TestCase):
 
         BlogPost.drop_collection()
 
+    def test_db_field_load(self):
+        """Ensure we load data correctly
+        """
+        class Person(Document):
+            name = StringField(required=True)
+            _rank = StringField(required=False, db_field="rank")
+
+            @property
+            def rank(self):
+                return self._rank or "Private"
+
+        Person.drop_collection()
+
+        Person(name="Jack", _rank="Corporal").save()
+
+        Person(name="Fred").save()
+
+        self.assertEquals(Person.objects.get(name="Jack").rank, "Corporal")
+        self.assertEquals(Person.objects.get(name="Fred").rank, "Private")
+
     def test_explicit_geo2d_index(self):
         """Ensure that geo2d indexes work when created via meta[indexes]
         """

.gitignore
@@ -13,5 +13,5 @@ env/
 .settings
 .project
 .pydevproject
-tests/bugfix.py
+tests/test_bugfix.py
 htmlcov/

tests/test_bugfix.py
@@ -1,47 +1,0 @@
-# import pickle
-# import pymongo
-# import bson
-# import warnings
-
-# from datetime import datetime
-
-# import tempfile
-# import pymongo, gridfs
-
-import unittest
-from mongoengine import *
-from bson.objectid import ObjectId
-
-class BugFixTest(unittest.TestCase):
-
-
-    def setUp(self):
-
-        conn = connect(db='mongoenginetest')
-
-    def test_items_list(self):
-
-        class ActivityType1(EmbeddedDocument):
-            activity_id = IntField()
-            activity_name = StringField()
-
-        class ActivityType2(EmbeddedDocument):
-            activity_id = IntField()
-            activity_status = StringField()
-
-        class UserActivities(Document):
-            user_id = IntField()
-            activity = GenericEmbeddedDocumentField(choices=(ActivityType1, ActivityType2))
-
-
-        UserActivities.drop_collection()
-
-        user_id = 123
-        activity_id = 321
-        UserActivities(user_id=user_id, activity=ActivityType2(activity_id=activity_id, activity_status="A")).save()
-
-        self.assertEquals(1, UserActivities.objects(user_id=user_id, __raw__={'activity.activity_status': 'A'}).count())
-
-
-
-

commit 662040887151e3002886f88e1a4377b926976e45

fix bug introduced in 9e7ea64
This fix caused embedded doc fields with db_field set to be overwritten
with their dictionary representation (note that this depended on the
ordering of keys in the values keyword arguments supplied to
BaseDocument.__init__ by BaseDocument._from_son).

mongoengine/base.py
@@ -798,7 +798,6 @@ class BaseDocument(object):
                     dynamic_data[key] = value
         else:
             for key, value in values.items():
-                key = self._reverse_db_field_map.get(key, key)
                 setattr(self, key, value)
 
         # Set any get_fieldname_display methods
@@ -957,6 +956,8 @@ class BaseDocument(object):
                 try:
                     data[field_name] = (value if value is None
                                     else field.to_python(value))
+                    if field_name != field.db_field:
+                        del data[field.db_field]
                 except (AttributeError, ValueError), e:
                     errors_dict[field_name] = e
             elif field.default:

tests/test_document.py
@@ -683,6 +683,29 @@ class DocumentTest(unittest.TestCase):
 
         self.assertEquals(Person.objects.get(name="Jack").rank, "Corporal")
         self.assertEquals(Person.objects.get(name="Fred").rank, "Private")
+    
+    def test_db_embedded_doc_field_load(self):
+        """Ensure we load embedded document data correctly
+        """
+        class Rank(EmbeddedDocument):
+            title = StringField(required=True)
+
+        class Person(Document):
+            name = StringField(required=True)
+            rank_ = EmbeddedDocumentField(Rank, required=False, db_field='rank')
+
+            @property
+            def rank(self):
+                return self.rank_.title if self.rank_ is not None else "Private"
+
+        Person.drop_collection()
+
+        Person(name="Jack", rank_=Rank(title="Corporal")).save()
+
+        Person(name="Fred").save()
+
+        self.assertEquals(Person.objects.get(name="Jack").rank, "Corporal")
+        self.assertEquals(Person.objects.get(name="Fred").rank, "Private")
 
     def test_explicit_geo2d_index(self):
         """Ensure that geo2d indexes work when created via meta[indexes]