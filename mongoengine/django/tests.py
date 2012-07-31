afdadf3188cc0d3bbd08c292fb00ea6b0cd347b4
Serge Matveenko <s@matveenko.ru>
2012-07-31 13:41:53 +0400

    Fix MongoTestCase crash. Use get_db function from connection module instead of defunct get_db connection class method.

--- mongoengine/django/tests.py
+++ mongoengine/django/tests.py
@@ -3,14 +3,17 @@ from django.test import TestCase
 from django.conf import settings
 
 from mongoengine import connect
+from mongoengine.connection import get_db
+
 
 class MongoTestCase(TestCase):
     """
     TestCase class that clear the collection between the tests
     """
     db_name = 'test_%s' % settings.MONGO_DATABASE_NAME
+
     def __init__(self, methodName='runtest'):
-        self.db = connect(self.db_name).get_db()
+        self.db = get_db()
         super(MongoTestCase, self).__init__(methodName)
 
     def _post_teardown(self):
