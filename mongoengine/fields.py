3090adac04d75020b9e89698ac7f29f1a31cdc1b
Ross Lawley <ross.lawley@gmail.com>
2012-09-24 11:37:54 +0000

    Fixed objectId for DBRef

--- mongoengine/fields.py
+++ mongoengine/fields.py
@@ -770,7 +770,7 @@ class ReferenceField(BaseField):
     def to_mongo(self, document):
         if isinstance(document, DBRef):
             if not self.dbref:
-                return "%s" % DBRef.id
+                return DBRef.id
             return document
         elif not self.dbref and isinstance(document, basestring):
             return document
