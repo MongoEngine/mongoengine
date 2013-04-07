81807c80af91f86035925434288e9276ecf385bc
AdallomRoyReznik <roy@adallom.com>
2013-04-07 10:36:38 +0300

    Revert "added groups, permissions and content types"
    This reverts commit 6db8d4c1f477061045e7ca54e9fc0feb6d6da115.

--- mongoengine/django/contenttypes.py
+++
@@ -1,6 +0,0 @@
-from mongoengine import *
-
-class ContentType(Document):
-    name = StringField(max_length=100)
-    app_label = StringField(max_length=100)
-    model = StringField(max_length=100)
