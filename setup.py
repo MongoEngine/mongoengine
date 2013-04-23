1e1e48732a92cc5e355301e7178b2337f2e48626
Ross Lawley <ross.lawley@gmail.com>
2013-04-23 15:33:38 +0000

    Update setup.py python versions / langauge in changelog

--- setup.py
+++ setup.py
@@ -38,7 +38,6 @@ CLASSIFIERS = [
     'Operating System :: OS Independent',
     'Programming Language :: Python',
     "Programming Language :: Python :: 2",
-    "Programming Language :: Python :: 2.5",
     "Programming Language :: Python :: 2.6",
     "Programming Language :: Python :: 2.7",
     "Programming Language :: Python :: 3",
