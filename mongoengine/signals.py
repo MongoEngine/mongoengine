04592c876b6ff6fb0c11338499ed4e0bf6934330
Alice Bevan-McGregor <alice@gothcandy.com>
2013-05-29 12:04:53 -0400

    Moved pre_save after validation and determination of creation state; added pre_save_validation where pre_save had been.

--- mongoengine/signals.py
+++ mongoengine/signals.py
@@ -38,6 +38,7 @@ _signals = Namespace()
 
 pre_init = _signals.signal('pre_init')
 post_init = _signals.signal('post_init')
+pre_save_validation = _signals.signal('pre_save_validation')
 pre_save = _signals.signal('pre_save')
 post_save = _signals.signal('post_save')
 pre_delete = _signals.signal('pre_delete')
