e36ded31dae68940f759bb58a6e759d22754cf8a
Stefan Wojcik <wojcikstefan@gmail.com>
2013-09-16 23:50:13 -0700

    fix validation for a nested DictField

--- tests/fields/fields.py
+++ tests/fields/fields.py
@@ -997,7 +997,13 @@ class FieldTest(unittest.TestCase):
         post.info = {'$title': 'test'}
         self.assertRaises(ValidationError, post.validate)
 
+        post.info = {'nested': {'$title': 'test'}}
+        self.assertRaises(ValidationError, post.validate)
+
         post.info = {'the.title': 'test'}
+        self.assertRaises(ValidationError, post.validate)
+
+        post.info = {'nested': {'the.title': 'test'}}
         self.assertRaises(ValidationError, post.validate)
 
         post.info = {1: 'test'}
