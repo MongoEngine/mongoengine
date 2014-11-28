d65ce6fc2c957d5b297fea724ad35ab0b34a138b
mrigal <matthieu@orderbird.com>
2014-11-28 13:54:33 +0100

    fixed bug for queryset.distinct to work also on embedded documents, not just on lists of embedded documents

--- mongoengine/queryset/base.py
+++ mongoengine/queryset/base.py
@@ -763,8 +763,7 @@ class BaseQuerySet(object):
 
             # We may need to cast to the correct type eg.
             # ListField(EmbeddedDocumentField)
-            doc_field = getattr(
-                self._document._fields.get(field), "field", None)
+            doc_field = getattr(self._document._fields.get(field), "field", self._document._fields.get(field))
             instance = getattr(doc_field, "document_type", False)
             EmbeddedDocumentField = _import_class('EmbeddedDocumentField')
             GenericEmbeddedDocumentField = _import_class(
