fef8d9b090aab9cf36a74e345151319b116d3d3e
mrigal <matthieu@orderbird.com>
2014-12-10 16:44:16 +0100

    added new test like defined in issue #712 and changed ObjectIdField to_python() method to use a try except similar to other Field classes

--- mongoengine/base/fields.py
+++ mongoengine/base/fields.py
@@ -402,8 +402,11 @@ class ObjectIdField(BaseField):
     """
 
     def to_python(self, value):
-        if not isinstance(value, ObjectId):
-            value = ObjectId(value)
+        try:
+            if not isinstance(value, ObjectId):
+                value = ObjectId(value)
+        except:
+            pass
         return value
 
     def to_mongo(self, value):
