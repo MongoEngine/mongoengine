a813d93403982e64f1f240464542166e9c0e6e0e
losintikfos <brightdadson@gmail.com>
2015-05-18 12:52:18 +0100

    Update fields.py
    Removed validators list default and set validators to None as default.

--- mongoengine/base/fields.py
+++ mongoengine/base/fields.py
@@ -44,7 +44,7 @@ class BaseField(object):
     def __init__(self, db_field=None, name=None, required=False, default=None,
                  unique=False, unique_with=None, primary_key=False,
                  validation=None, choices=None, verbose_name=None,
-                 help_text=None, null=False, sparse=False, validators=[]):
+                 help_text=None, null=False, sparse=False, validators=None):
         """
         :param db_field: The database field to store this field in
             (defaults to the name of the field)
