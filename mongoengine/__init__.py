c6151e34e0a3f87c5d8ceb409c968a1f8cdee4a6
Matthieu Rigal <matt.rigal@gmail.com>
2015-06-24 12:39:21 +0200

    Bumped version to 0.10.0

--- mongoengine/__init__.py
+++ mongoengine/__init__.py
@@ -14,7 +14,7 @@ import errors
 __all__ = (list(document.__all__) + fields.__all__ + connection.__all__ +
            list(queryset.__all__) + signals.__all__ + list(errors.__all__))
 
-VERSION = (0, 9, 0)
+VERSION = (0, 10, 0)
 
 
 def get_version():
