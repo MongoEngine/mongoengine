09dbf20cec19b1ce65af1ad8f73fb32a85fda068
Andrey Dresvyannikov <andrey.dre@mindojo.com>
2016-01-19 10:48:26 +0300

    fix failing pymongo 2.7 tests while no insert_one method

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,5 +1,10 @@
+import pymongo
+
 from all_warnings import AllWarnings
 from document import *
 from queryset import *
 from fields import *
 from migration import *
+
+if not hasattr(pymongo.collection.Collection, 'insert_one'):
+    setattr(pymongo.collection.Collection, 'insert_one', pymongo.collection.Collection.insert)
