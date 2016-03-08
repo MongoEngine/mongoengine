dc03a6c0964678f65fef77e298004971f907fd09
Manuel Schraner <manuel.schraner@gmail.com>
2016-03-08 12:35:17 +0100

    python 3 code is handled by 2to3; test for db_field translation to and from SON.

--- mongoengine/base/document.py
+++ mongoengine/base/document.py
@@ -686,12 +686,7 @@ class BaseDocument(object):
         # get the class name from the document, falling back to the given
         # class if unavailable
         class_name = son.get('_cls', cls._class_name)
-        if PY3:
-            son_gen = son.items()
-        else:
-            son_gen = son.iteritems()
-
-        data = dict(("%s" % cls._db_field_map.get(key,key), value) for key, value in son_gen)
+        data = dict(("%s" % cls._db_field_map.get(key,key), value) for key, value in son.iteritems())
 
         # Return correct subclass for document type
         if class_name != cls._class_name:
--- tests/document/db_fields.py
+++ tests/document/db_fields.py
@@ -2,6 +2,8 @@
 import sys
 sys.path[0:0] = [""]
 import unittest
+
+from bson import SON
 from mongoengine import *
 
 __all__ = ("DbFieldParameterTest", )
@@ -9,11 +11,10 @@ __all__ = ("DbFieldParameterTest", )
 
 class DbFieldParameterTest(unittest.TestCase):
 
-    def test_document_db_field_validation(self):
-
-        # https://github.com/MongoEngine/mongoengine/issues/904
-
-        data = {'b': {'c': [{'x': 1.0, 'y': 2.0}]}}
+    def setUp(self):
+        # testcase from https://github.com/MongoEngine/mongoengine/issues/904
+        self.data = {'b': {'c': [{'x': 1.0, 'y': 2.0}]}}
+        self.reference_son = SON([('fb', SON([('fc', [SON([('fx', 1.0), ('fy', 2.0)])])]))])
 
         class C(EmbeddedDocument):
             x = FloatField(db_field='fx')
@@ -25,12 +26,23 @@ class DbFieldParameterTest(unittest.TestCase):
         class A(Document):
             b = EmbeddedDocumentField(B, db_field='fb')
 
-        a = A(**data)
+        self.A = A
 
+    def test_document_db_field_validation(self):
+        a = self.A(**self.data)
         try:
             a.validate()
         except ValidationError as e:
             self.fail("ValidationError raised: %s" % e.message)
+
+    def test_document_fieldname_to_db_field_name(self):
+        a = self.A(**self.data)
+        self.assertEqual(a.to_mongo(), self.reference_son)
+
+    def test_document_db_fieldname_to_fieldname(self):
+        a = self.A._from_son(self.reference_son)
+        self.assertEqual(a.b.c[0].x, 1.0)
+        self.assertEqual(a.b.c[0].y, 2.0)
 
 
 if __name__ == '__main__':
