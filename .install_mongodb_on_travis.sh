cb62b457bc788b36539bda479613761a893447cd
Stefan Wojcik <wojcikstefan@gmail.com>
2017-02-25 18:21:04 -0500

    fix mongo install script

--- .install_mongodb_on_travis.sh
+++ .install_mongodb_on_travis.sh
@@ -9,10 +9,12 @@ if [ "$MONGODB" = "2.4" ]; then
     # No need for `sudo service mongodb start` because v2.4 starts automatically
 elif [ "$MONGODB" = "2.6" ]; then
     echo "deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen" | sudo tee /etc/apt/sources.list.d/mongodb.list
+    sudo apt-get update
     sudo apt-get install mongodb-org-server=2.6.12
     sudo service mongodb start
 elif [ "$MONGODB" = "3.0" ]; then
     echo "deb http://repo.mongodb.org/apt/ubuntu precise/mongodb-org/3.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb.list
+    sudo apt-get update
     sudo apt-get install mongodb-org-server=3.0.14
     sudo service mongodb start
 else
