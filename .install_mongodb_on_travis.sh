098a90ee962393fad379b75d0b4c957a7bd97517
Stefan Wojcik <wojcikstefan@gmail.com>
2017-02-25 18:32:08 -0500

    no need for service start

--- .install_mongodb_on_travis.sh
+++ .install_mongodb_on_travis.sh
@@ -6,17 +6,14 @@ if [ "$MONGODB" = "2.4" ]; then
     echo "deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen" | sudo tee /etc/apt/sources.list.d/mongodb.list
     sudo apt-get update
     sudo apt-get install mongodb-10gen=2.4.14
-    # No need for `sudo service mongodb start` because v2.4 starts automatically
 elif [ "$MONGODB" = "2.6" ]; then
     echo "deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen" | sudo tee /etc/apt/sources.list.d/mongodb.list
     sudo apt-get update
     sudo apt-get install mongodb-org-server=2.6.12
-    sudo service mongodb start
 elif [ "$MONGODB" = "3.0" ]; then
     echo "deb http://repo.mongodb.org/apt/ubuntu precise/mongodb-org/3.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb.list
     sudo apt-get update
     sudo apt-get install mongodb-org-server=3.0.14
-    sudo service mongodb start
 else
     echo "Invalid MongoDB version, expected 2.4, 2.6, or 3.0."
     exit 1
