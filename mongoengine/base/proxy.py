13511ef2965b7ef4e972ee0e79deeef071de3d42
sauravshah <sauravshah@gmail.com>
2017-05-15 16:46:01 +0530

    Make __call__ return the object

--- mongoengine/base/proxy.py
+++ mongoengine/base/proxy.py
@@ -3,6 +3,11 @@ import lazy_object_proxy
 
 class DocumentProxy(lazy_object_proxy.Proxy):
     id = None
+    wrapped = None
     def __init__(self, wrapped, id):
         super(DocumentProxy, self).__init__(wrapped)
         self.id = id
+
+    def __call__(self, *args, **kwargs):
+        # Hack as callable(lazy_object_proxy.Proxy) return True
+        return self.__wrapped__
