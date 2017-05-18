bb46e4d611cd8828f722856920c4106c60033093
Danil Eremeev <eremeev-dy@crona.ru>
2017-05-18 16:40:37 +0500

    to_db_fields fix

--- mongoengine/queryset/base.py
+++ mongoengine/queryset/base.py
@@ -1731,14 +1731,14 @@ class BaseQuerySet(object):
                           for x in document._subclasses][1:]
         for field in fields:
             try:
-                field = '.'.join(f.db_field for f in
+                field = '.'.join(f.db_field if not isinstance(f, six.string_types) else f for f in
                                  document._lookup_field(field.split('.')))
                 ret.append(field)
             except LookUpError as err:
                 found = False
                 for subdoc in subclasses:
                     try:
-                        subfield = '.'.join(f.db_field for f in
+                        subfield = '.'.join(f.db_field if not isinstance(f, six.string_types) else f for f in
                                             subdoc._lookup_field(field.split('.')))
                         ret.append(subfield)
                         found = True
