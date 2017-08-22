36918557a6d487b75aa885ce2e367c9e2f9606bd
Erdenezul <erdenezul@shine.mn>
2017-08-22 19:47:52 +0900

    github travis system updated to trusty

--- .install_mongodb_on_travis.sh
+++ .install_mongodb_on_travis.sh
@@ -13,7 +13,7 @@ elif [ "$MONGODB" = "2.6" ]; then
     sudo apt-get install mongodb-org-server=2.6.12
     # service should be started automatically
 elif [ "$MONGODB" = "3.0" ]; then
-    echo "deb http://repo.mongodb.org/apt/ubuntu precise/mongodb-org/3.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb.list
+    echo "deb http://repo.mongodb.org/apt/ubuntu trusty/mongodb-org/3.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb.list
     sudo apt-get update
     sudo apt-get install mongodb-org-server=3.0.14
     # service should be started automatically
