7efa67e7e6a6bf32d208c6628b3b43a4c0d89bab
Ivan Pogrebkov <chezhik@gmail.com>
2018-02-05 12:35:06 +0300

    reverse to 'style fix'

--- mongoengine/document.py
+++ mongoengine/document.py
@@ -1010,6 +1010,7 @@ class DynamicDocument(Document):
         field_name = args[0]
         if field_name in self._dynamic_fields:
             setattr(self, field_name, None)
+            self._dynamic_fields[field_name].null = False
         else:
             super(DynamicDocument, self).__delattr__(*args, **kwargs)
 
