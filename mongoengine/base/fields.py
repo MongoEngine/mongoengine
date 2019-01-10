44d88e3b7cb1d7694d8fc1162455eff36a47817d
Prasanna Sankaranarayanan <myprasanna@gmail.com>
2019-01-10 23:50:38 +0000

    Fix only fields attr error.

--- mongoengine/base/fields.py
+++ mongoengine/base/fields.py
@@ -112,10 +112,13 @@ class BaseField(object):
         if instance is None:
             # Document class being used rather than a document object
             return self
-            
-        if instance._only_fields and (self.db_field or self.name) not in instance._only_fields:
-            instance._only_fields = None
-            instance.reload()
+        
+        try:
+            if instance._only_fields and (self.db_field or self.name) not in instance._only_fields:
+                instance._only_fields = None
+                instance.reload()
+        except AttributeError:
+            pass
 
         if self.is_v2_field():
             return self.v2_get(instance)
