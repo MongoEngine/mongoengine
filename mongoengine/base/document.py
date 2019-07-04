dc4b36fa04047e7d4c4f97c192dc4cb5d430f577
sudiksha.nanda <sudiksha@rippling.com>
2019-07-04 12:16:19 +0530

    explore

--- mongoengine/base/document.py
+++ mongoengine/base/document.py
@@ -879,6 +879,8 @@ class BaseDocument(object):
         else:
             # append an index on history_date,
             # _rippling_process_index_spec will take care of adding company prefix if needed
+            indexes.append({'fields': [('history_date', -1)]})
+
             indexes.append({'fields': [('history_date', -1)],
                             'args': {'noCompanyPrefix': True}})
         return indexes
