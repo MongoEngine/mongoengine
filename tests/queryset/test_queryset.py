bc77322c2f166e28390e1492a5a67440f90af021
Agustin Barto <abarto@gmail.com>
2020-03-17 21:49:17 -0300

    Update unit tests

--- tests/queryset/test_queryset.py
+++ tests/queryset/test_queryset.py
@@ -4739,33 +4739,33 @@ class TestQueryset(unittest.TestCase):
         bars = list(Bar.objects.read_concern(None))
         assert bars == [bar]
 
-        bars = Bar.objects.read_concern(ReadConcern(level="local"))
-        assert bars._read_concern == ReadConcern(level="local")
-        assert bars._cursor.collection.read_concern == ReadConcern(level="local")
+        bars = Bar.objects.read_concern({"level": "local"})
+        assert bars._read_concern.document == {"level": "local"}
+        assert bars._cursor.collection.read_concern.document == {"level": "local"}
 
-        # Make sure that `.read_concern(...)` does accept string values.
+        # Make sure that `.read_concern(...)` does not accept string values.
         with pytest.raises(TypeError):
             Bar.objects.read_concern("local")
 
         def assert_read_concern(qs, expected_read_concern):
-            assert qs._read_concern == expected_read_concern
-            assert qs._cursor.collection.read_concern == expected_read_concern
+            assert qs._read_concern.document == expected_read_concern
+            assert qs._cursor.collection.read_concern.document == expected_read_concern
 
         # Make sure read concern is respected after a `.skip(...)`.
-        bars = Bar.objects.skip(1).read_concern(ReadConcern("majority"))
-        assert_read_concern(bars, ReadConcern("majority"))
+        bars = Bar.objects.skip(1).read_concern({"level": "local"})
+        assert_read_concern(bars, {"level": "local"})
 
         # Make sure read concern is respected after a `.limit(...)`.
-        bars = Bar.objects.limit(1).read_concern(ReadConcern("majority"))
-        assert_read_concern(bars, ReadConcern("majority"))
+        bars = Bar.objects.limit(1).read_concern({"level": "local"})
+        assert_read_concern(bars, {"level": "local"})
 
         # Make sure read concern is respected after an `.order_by(...)`.
-        bars = Bar.objects.order_by("txt").read_concern(ReadConcern("majority"))
-        assert_read_concern(bars, ReadConcern("majority"))
+        bars = Bar.objects.order_by("txt").read_concern({"level": "local"})
+        assert_read_concern(bars, {"level": "local"})
 
         # Make sure read concern is respected after a `.hint(...)`.
-        bars = Bar.objects.hint([("txt", 1)]).read_concern(ReadConcern("majority"))
-        assert_read_concern(bars, ReadConcern("majority"))
+        bars = Bar.objects.hint([("txt", 1)]).read_concern({"level": "majority"})
+        assert_read_concern(bars, {"level": "majority"})
 
     def test_json_simple(self):
         class Embedded(EmbeddedDocument):
