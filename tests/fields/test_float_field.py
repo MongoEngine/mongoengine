ef1ddbbfaa6f1d87addfa6e0ac28ace52d5b0a7e
Adam Edry <aedry.cs@gmail.com>
2020-04-05 15:53:15 -0500

    Add failing tests for using None values in a list of floats

--- tests/fields/test_float_field.py
+++ tests/fields/test_float_field.py
@@ -20,6 +20,35 @@ class TestFloatField(MongoDBTestCase):
         assert 1 == TestDocument.objects(float_fld__ne=None).count()
         assert 1 == TestDocument.objects(float_fld__ne=1).count()
 
+    def test_list_of_floats_can_be_none(self):
+        class TestFloatListNone(Document):
+            list_of_float_fld = ListField(FloatField())
+
+        TestFloatListNone.drop_collection()
+
+        # Verifies that validate works with None
+        TestFloatListNone(list_of_float_fld=[None]).save()
+
+    def test_list_of_floats_can_update_none(self):
+        class TestFloatListUpdateNone(Document):
+            list_of_float_fld = ListField(FloatField())
+
+        TestFloatListUpdateNone.drop_collection()
+
+        doc = TestFloatListUpdateNone()
+        doc.list_of_float_fld = [1.2]
+        doc.save()
+
+        # Verifies that to_python works with None
+        res = TestFloatListUpdateNone.objects(id=doc.id).update(
+            set__list_of_float_fld=[None],
+        )
+        assert res == 1
+
+        # Retrive data from db and verify it.
+        ret = TestFloatListUpdateNone.objects.all()[0]
+        assert ret.list_of_float_fld == [None]
+
     def test_validation(self):
         """Ensure that invalid values cannot be assigned to float fields.
         """
