420728d25c523e535778c6d9e705ba96778885b1
Manuel Avalos <manuelavalos97@gmail.com>
2020-06-08 19:20:20 -0500

    Adding EnumField to fields file

--- mongoengine/fields.py
+++ mongoengine/fields.py
@@ -2179,6 +2179,43 @@ class SequenceField(BaseField):
         return value
 
 
+class EnumField(BaseField):
+    """
+    https://github.com/MongoEngine/extras-mongoengine/blob/master/
+    extras_mongoengine/fields.py
+    A class to register Enum type (from the package enum34) into mongo
+    :param choices: must be of :class:`enum.Enum`: type
+        and will be used as possible choices
+    """
+
+    def __init__(self, enum, *args, **kwargs):
+        self.enum = enum
+        kwargs['choices'] = [choice for choice in enum]
+        super(EnumField, self).__init__(*args, **kwargs)
+
+    def __get_value(self, enum):
+        return enum.value if hasattr(enum, 'value') else enum
+
+    def to_python(self, value):
+        return self.enum(super(EnumField, self).to_python(value))
+
+    def to_mongo(self, value):
+        return self.__get_value(value)
+
+    def prepare_query_value(self, op, value):
+        return super(EnumField, self).prepare_query_value(
+            op, self.__get_value(value)
+        )
+
+    def validate(self, value):
+        return super(EnumField, self).validate(self.__get_value(value))
+
+    def _validate(self, value, **kwargs):
+        return super(EnumField, self)._validate(
+            self.enum(self.__get_value(value)), **kwargs
+        )
+
+
 class UUIDField(BaseField):
     """A UUID field.
 
