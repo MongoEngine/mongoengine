76cc308176eddd0a8557eb7c0c17a4c662363e44
Sachith Gullapalli <sgullapalli@rippling.com>
2020-10-29 15:14:27 -0400

    take two at eliminating dbref lookups for iterable query params

--- mongoengine/queryset/visitor.py
+++ mongoengine/queryset/visitor.py
@@ -64,8 +64,14 @@ class SimplificationVisitor(QNodeVisitor):
             for op in ops:
                 from mongoengine import Document
                 from mongoengine.base.proxy import DocumentProxy
-                if type(query[op]) is DocumentProxy or isinstance(query[op], Document):
+                import collections
+                def is_doc_or_proxy(val):
+                    return type(val) is DocumentProxy or isinstance(val, Document)
+                if is_doc_or_proxy(query[op]):
                     query[op] = query[op].id
+                elif isinstance(query[op], collections.Iterable):
+                    convert_to_id = lambda obj: obj.id if is_doc_or_proxy(obj) else obj
+                    query[op] = list(map(convert_to_id, query[op]))
             
             combined_query.update(copy.deepcopy(query))
             
