0620ac5641b721e2205647d67718a1ae329d1bac
Bastien Gerard <bast.gerard@gmail.com>
2021-03-03 11:30:03 +0100

    Fix mongo download link as convention changed in official repo with > 4.0

--- .github/workflows/install_mongo.sh
+++ .github/workflows/install_mongo.sh
@@ -2,7 +2,14 @@
 
 MONGODB=$1
 
+# Mongo > 4.0 follows different name convention for download links
 mongo_build=mongodb-linux-x86_64-${MONGODB}
+
+if [[ "$MONGODB" == *"4."* ]] && [[ ! "$MONGODB" == *"4.0"* ]]; then
+  echo "It's there."
+  mongo_build=mongodb-linux-x86_64-ubuntu2004-v${MONGODB}-latest
+fi
+
 wget http://fastdl.mongodb.org/linux/$mongo_build.tgz
 tar xzf $mongo_build.tgz
 ${PWD}/$mongo_build/bin/mongod --version
