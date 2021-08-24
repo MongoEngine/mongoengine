c373b27f6bd57a0da2b994dd5da3c7a2e78825f4
Sachith Gullapalli <sgullapalli@rippling.com>
2021-08-23 20:15:27 -0700

    tweak import

--- django_mongoengine-stubs/__init__.pyi
+++ django_mongoengine-stubs/__init__.pyi
@@ -1,3 +1,3 @@
-from .queryset import QuerySet
-from .document import Document, DynamicDocument, EmbeddedDocument
+from django_mongoengine.queryset import QuerySet
+from django_mongoengine.document import Document, DynamicDocument, EmbeddedDocument
 __all__ = ["QuerySet", "Document", "DynamicDocument", "EmbeddedDocument"]
