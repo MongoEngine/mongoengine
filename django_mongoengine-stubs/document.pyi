6c914e0c6ac44c487f8eb7aa94c87963a0f3e135
Sachith Gullapalli <sgullapalli@rippling.com>
2021-08-23 19:54:11 -0700

    update

--- django_mongoengine-stubs/document.pyi
+++ django_mongoengine-stubs/document.pyi
@@ -1,2 +1,10 @@
-from mongoengine.document import Document, DynamicDocument, EmbeddedDocument
-__all__ = ('Document', 'DynamicDocument', 'EmbeddedDocument')
+from mongoengine import document as md
+from .queryset import QuerySetManager
+
+class DjangoFlavor(object):
+    objects = QuerySetManager()
+
+# ignoring mixin narrowing in override that mypy doesn't like
+class Document(DjangoFlavor, md.Document): ... # type: ignore
+class EmbeddedDocument(DjangoFlavor, md.EmbeddedDocument): ... # type: ignore
+class DynamicDocument(DjangoFlavor, md.DynamicDocument): ... # type: ignore
