f21f6cd3a53935dbce21a5fb43a4c5ba2d9877a7
Sachith Gullapalli <sgullapalli@rippling.com>
2021-08-23 19:34:21 -0700

    retry querysetmanager impl

---
+++ django_mongoengine-stubs/__init__.pyi
@@ -0,0 +1,3 @@
+from .queryset import QuerySet
+from .document import Document, DynamicDocument, EmbeddedDocument
+__all__ = ["QuerySet", "Document", "DynamicDocument", "EmbeddedDocument"]
---
+++ django_mongoengine-stubs/document.pyi
@@ -0,0 +1,2 @@
+from mongoengine.document import Document, DynamicDocument, EmbeddedDocument
+__all__ = ('Document', 'DynamicDocument', 'EmbeddedDocument')
