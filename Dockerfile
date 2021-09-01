9d2c0530eb46415a4a7d55f296a64be3800bff33
Juan Gutierrez <juanny.gee@gmail.com>
2021-09-01 16:55:36 -0400

    Remove docker refs from other branch

--- Dockerfile
+++
@@ -1,5 +0,0 @@
-FROM mongo:4
-
-COPY ./entrypoint.sh entrypoint.sh
-RUN chmod u+x entrypoint.sh
-ENTRYPOINT ./entrypoint.sh
--- entrypoint.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/bash
-
-mongod --replSet mongoengine --fork --logpath=/var/log/mongodb.log
-mongo db --eval "rs.initiate()"
-mongod --shutdown
-mongod --replSet mongoengine --bind_ip 0.0.0.0
