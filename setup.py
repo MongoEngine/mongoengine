e80bf741b8a6a3110c1fc4b748d2744b09b1fadc
Bastien Gerard <bast.gerard@gmail.com>
2022-12-27 22:31:12 +0100

    use ubuntu20 as mongo installation broke with ubuntu-latest (now 22)

--- setup.py
+++ setup.py
@@ -141,7 +141,7 @@ setup(
     long_description=LONG_DESCRIPTION,
     platforms=["any"],
     classifiers=CLASSIFIERS,
-    python_requires=">=3.6",
+    python_requires=">=3.7",
     install_requires=["pymongo>=3.4,<5.0"],
     cmdclass={"test": PyTest},
     **extra_opts
