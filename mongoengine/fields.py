0773c993198217b58d8983b636bfcae514a409f6
Bastien Gerard <bast.gerard@gmail.com>
2023-02-25 22:46:27 +0100

    Simplify LongField implementation

--- mongoengine/fields.py
+++ mongoengine/fields.py
@@ -356,45 +356,11 @@ class IntField(BaseField):
         return super().prepare_query_value(op, int(value))
 
 
-class LongField(BaseField):
+class LongField(IntField):
     """64-bit integer field. (Equivalent to IntField since the support to Python2 was dropped)"""
-
-    def __init__(self, min_value=None, max_value=None, **kwargs):
-        """
-        :param min_value: (optional) A min value that will be applied during validation
-        :param max_value: (optional) A max value that will be applied during validation
-        :param kwargs: Keyword arguments passed into the parent :class:`~mongoengine.BaseField`
-        """
-        self.min_value, self.max_value = min_value, max_value
-        super().__init__(**kwargs)
-
-    def to_python(self, value):
-        try:
-            value = int(value)
-        except (TypeError, ValueError):
-            pass
-        return value
 
     def to_mongo(self, value):
         return Int64(value)
-
-    def validate(self, value):
-        try:
-            value = int(value)
-        except (TypeError, ValueError):
-            self.error("%s could not be converted to long" % value)
-
-        if self.min_value is not None and value < self.min_value:
-            self.error("Long value is too small")
-
-        if self.max_value is not None and value > self.max_value:
-            self.error("Long value is too large")
-
-    def prepare_query_value(self, op, value):
-        if value is None:
-            return value
-
-        return super().prepare_query_value(op, int(value))
 
 
 class FloatField(BaseField):
