2bf863eb02bbe0e82191a75254ade4ccab8c4557
Juan Gutierrez <juanny.gee@gmail.com>
2024-09-11 20:33:23 -0400

    Mongo3.8 does not support max tx lock param

--- .github/workflows/start_mongo.sh
+++ .github/workflows/start_mongo.sh
@@ -6,13 +6,12 @@ mongodb_dir=$(find ${PWD}/ -type d -name "mongodb-linux-x86_64*")
 
 mkdir $mongodb_dir/data
 
-$mongodb_dir/bin/mongod \
- --dbpath $mongodb_dir/data \
- --logpath $mongodb_dir/mongodb.log \
- --fork \
- --setParameter maxTransactionLockRequestTimeoutMillis=1000 \
- --replSet \
- mongoengine
+args=(--dbpath $mongodb_dir/data --logpath $mongodb_dir/mongodb.log --fork --replSet mongoengine)
+if (( $(echo "$MONGODB > 3.8" | bc -l) )); then
+    args+=(--setParameter maxTransactionLockRequestTimeoutMillis=1000)
+fi
+
+$mongodb_dir/bin/mongod "${args[@]}"
 
 if (( $(echo "$MONGODB < 6.0" | bc -l) )); then
 mongo --verbose --eval "rs.initiate()"
