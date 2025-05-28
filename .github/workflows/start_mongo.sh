1fcbad8e8395eef31a4a08b4e16540ca8dbaa534
Terence D. Honles <terence@honles.com>
2025-05-28 08:48:22 +0200

    fix CI

--- .github/workflows/start_mongo.sh
+++ .github/workflows/start_mongo.sh
@@ -1,14 +1,25 @@
 #!/bin/bash
 
+set -e  # Exit immediately if a command exits with a non-zero status
+set -u  # Treat unset variables as an error
+
+. mongo.path
+
 MONGODB=$1
 
-mongodb_dir=$(find ${PWD}/ -type d -name "mongodb-linux-x86_64*")
+mongodb_dir=$(find ${PWD}/ -type d -name "mongodb-linux-x86_64*" -print -quit)
 
 mkdir $mongodb_dir/data
-$mongodb_dir/bin/mongod --dbpath $mongodb_dir/data --logpath $mongodb_dir/mongodb.log --fork
 
-if (( $(echo "$MONGODB < 6.0" | bc -l) )); then
-mongo --quiet --eval 'db.runCommand("ping").ok'    # Make sure mongo is awake
-else
-mongosh --quiet  --eval 'db.runCommand("ping").ok'  # Make sure mongo is awake
+args=(--dbpath $mongodb_dir/data --logpath $mongodb_dir/mongodb.log --fork --replSet mongoengine)
+
+# Parse version components
+MAJOR=$(echo "$MONGODB" | cut -d'.' -f1)
+MINOR=$(echo "$MONGODB" | cut -d'.' -f2)
+if [ "$MAJOR" -gt 3 ] || ([ "$MAJOR" -eq 3 ] && [ "$MINOR" -ge 8 ]); then
+    args+=(--setParameter maxTransactionLockRequestTimeoutMillis=1000)
 fi
+
+mongod "${args[@]}"
+mongosh --verbose --eval "rs.initiate()"
+mongosh --quiet --eval "rs.status().ok"
